1a6d2add4085470628790ded343bd14497792920
Suhel <suhel7822@gmail.com>
2024-03-16 00:21:38 +0530

    users middleware is created

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+
+import users from './users';
 import authentication from './authentication';
 
 const router = express.Router();
 
 export default (): express.Router =>{
-    authentication(router)
+    authentication(router);
+    users(router);
+    
     return router;
 }
