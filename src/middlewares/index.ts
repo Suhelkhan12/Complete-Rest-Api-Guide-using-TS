86dba6a64ae2be8a688391ac7ada0fa8ed56a21b
Suhel <suhel7822@gmail.com>
2024-03-16 00:27:49 +0530

    bug in delete user resolved

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,7 +1,29 @@
 import express from 'express'
 import {get, identity, merge} from 'lodash'
 
-import { getUserBySessionToken } from '../db/users'
+import { getUserBySessionToken } from '../db/users';
+
+export const isOwner =  async (req:express.Request, res:express.Response, next:express.NextFunction)=>{
+    try{
+
+        const {id} = req.params;
+        const currentUserId = get(req, 'identity._id') as string;
+
+        if(!currentUserId){
+            return res.sendStatus(403);
+        }
+
+        if(currentUserId.toString() !== id){
+            return res.sendStatus(403)
+        }
+
+        next();
+
+    }catch(err){
+        console.log(err);
+        return res.sendStatus(400)
+    }
+}
 
 export const isAuthenticated = async (req:express.Request, res:express.Response, next:express.NextFunction)=>{
     try{
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 
 import { deleteUser, getAllUsers } from '../controllers/users';
-import { isAuthenticated } from '../middlewares';
+import { isAuthenticated, isOwner, isOwner } from '../middlewares';
 
 export default (router: express.Router)=>{
     router.get('/users',isAuthenticated,getAllUsers);
-    router.delete('/users/:id', deleteUser)
+    router.delete('/users/:id',isOwner, deleteUser)
 }
